cacaf4d7d3f23dfd4992d98ebc1972e015b41133
Tamanna <tamannaemails@gmail.com>
2026-08-15 23:56:03 +0600

    changed

--- src/app/module/auth/auth.validation.ts
+++ src/app/module/auth/auth.validation.ts
@@ -41,12 +41,12 @@ const ResetPasswordZodSchema = z.object({
 	email: z.email(),
 	newPassword: z
 		.string()
-		.min(8, "Password Must Minimum 8 Characters Long.")
-		.regex(/[a-z]/, "Password must contain atleast 1 Lowercase Letter")
-		.regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter")
+		.min(8, "Password Must Minimum 8 Characters Long...")
+		.regex(/[a-z]/, "Password must contain atleast 1 Lowercase Letter...")
+		.regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter...")
 
-		.regex(/[0-9]/, "Password must contain atleast 1 Number")
-		.regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character"),
+		.regex(/[0-9]/, "Password must contain atleast 1 Number...")
+		.regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character..."),
 	otp: z.string().length(6),
 });
 
