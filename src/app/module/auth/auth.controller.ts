f2fea13f5bcf8dffa1dcb969271a3fc88fbb400c
Tamanna <tamannaemails@gmail.com>
2026-08-16 23:30:18 +0600

    updated forgot pass func

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -143,18 +143,7 @@ const googleLogin = catchAsync(async (req: Request, res: Response) => {
 		},
 	});
 });
-const forgotPassword = catchAsync(async (req: Request, res: Response) => {
-	const payload = req.body;
 
-	await AuthService.forgotPassword(payload);
-
-	sendResponse(res, {
-		statusCode: httpStatus.OK,
-		success: true,
-		message: `OTP Sent To Email : ${payload.email}`,
-		data: null,
-	});
-});
 const resetPassword = catchAsync(async (req: Request, res: Response) => {
 	const payload = req.body;
 
